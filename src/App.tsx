e83f0128539e4c09894730b9375a56eb420c9722
gigakhrz <kharazishvilig13@gmail.com>
2024-06-14 10:20:53 +0400

    add some default styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+import Header from "./components/Header";
+
 function App() {
-  return <></>;
+  return (
+    <AppWrapper>
+      <Header />
+    </AppWrapper>
+  );
 }
 
 export default App;
+
+const AppWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  background-color: #f2f2f2;
+`;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,17 @@
+import styled from "styled-components";
+
 const Header = () => {
-  return <div>Header</div>;
+  return <HeaderContainer>Header</HeaderContainer>;
 };
 
 export default Header;
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  height: 80px;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
