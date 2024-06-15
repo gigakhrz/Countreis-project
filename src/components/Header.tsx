305450dab8898eeefc1b801801975f3c4a5d44d0
gigakhrz <kharazishvilig13@gmail.com>
2024-06-15 16:35:21 +0400

    add moon and sun icons, and fucntion to change mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,28 @@
 import styled from "styled-components";
-import moon from "../../public/moon.png";
+import moon from "../../src/assets/icon-moon.svg";
+import sun from "../../src/assets/icon-sun.svg";
+
+import { useRestCountriesStore } from "../states/store";
 
 const Header = () => {
+  //state
+  const darkMode = useRestCountriesStore((store) => store.darkMode);
+
+  //setState
+  const setDarkmode = useRestCountriesStore((store) => store.setDarkMode);
+
+  console.log(darkMode);
   return (
-    <HeaderContainer>
+    <HeaderContainer darkMode={darkMode}>
       <p className="title">Where in the world?</p>
       <div className="selectMode">
-        <img className="moon" src={moon} alt="moon icon" />
-        <p className="dark">Dark mode</p>
+        <img
+          className="moon"
+          src={darkMode ? sun : moon}
+          alt="moon icon"
+          onClick={() => setDarkmode(!darkMode)}
+        />
+        <p className="dark">Mode</p>
       </div>
     </HeaderContainer>
   );
@@ -15,10 +30,10 @@ const Header = () => {
 
 export default Header;
 
-const HeaderContainer = styled.div`
+const HeaderContainer = styled.div<{ darkMode: boolean }>`
   width: 100%;
   height: 80px;
-  background-color: white;
+  background-color: ${({ darkMode }) => (darkMode ? "black" : "white")};
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -27,6 +42,7 @@ const HeaderContainer = styled.div`
   .title {
     font-size: 15px;
     font-weight: bold;
+    color: ${(props) => (props.darkMode ? "white" : "black")};
   }
 
   .selectMode {
@@ -37,12 +53,13 @@ const HeaderContainer = styled.div`
     gap: 5px;
 
     .moon {
-      width: 12px;
-      height: 12px;
+      width: 15px;
+      height: 15px;
     }
 
     .dark {
       font-size: 13px;
+      color: ${(props) => (props.darkMode ? "white" : "black")};
     }
   }
 `;
