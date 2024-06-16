dfa1fcd1815d042038afb708cbff7b01bcddd22f
gigakhrz <kharazishvilig13@gmail.com>
2024-06-16 09:27:04 +0400

    add default styles in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
+import styled from "styled-components";
+
 const Home = () => {
-  return <div>Home</div>;
+  return <HomeWrapper>Home</HomeWrapper>;
 };
 
 export default Home;
+
+const HomeWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
