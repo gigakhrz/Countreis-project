6d911aab9643ce84e163670eb0ede1a262c82823
gigakhrz <kharazishvilig13@gmail.com>
2024-06-16 11:04:21 +0400

    add axios request to catch thne information which will be relative for that project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import styled from "styled-components";
 import Header from "./components/Header";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await axios.get(
+          "https://restcountries.com/v3.1/all?fields=name,nativeName,population,region,subregion,capital,tld,currencies,languages"
+        );
+        const data = response.data;
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchCountries();
+  }, []);
   return (
     <AppWrapper>
       <Header />
