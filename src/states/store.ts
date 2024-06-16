a663ccfb3a6138dfe5f873e467a223a20d45d2a1
gigakhrz <kharazishvilig13@gmail.com>
2024-06-16 17:54:25 +0400

    add countrisInfo states

--- src/states/store.ts
+++ src/states/store.ts
@@ -1,20 +1,26 @@
 import { create } from "zustand";
+import countriesType from "../../type";
 
 // types for states and satstates
 type stateStore = {
   //states
   darkMode: boolean;
-
+  countriesInfo: countriesType[];
   //setStates
   setDarkMode: (mode: boolean) => void;
+  setCountriesInfo: (info: countriesType[]) => void;
 };
 
 export const useRestCountriesStore = create<stateStore>((set) => ({
   // states
   darkMode: false,
+  countriesInfo: [],
 
   //setStates
   setDarkMode: (mode: boolean) => {
     set({ darkMode: mode });
+  },
+  setCountriesInfo: (info: countriesType[]) => {
+    set({ countriesInfo: info });
   },
 }));
