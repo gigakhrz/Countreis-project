03abfce2933b16353fc40f5f27538f9b32034d38
gigakhrz <kharazishvilig13@gmail.com>
2024-06-16 11:05:58 +0400

    add countries's data type

---
+++ type.ts
@@ -0,0 +1,26 @@
+type countriesData = {
+  name: {
+    common: "Wallis and Futuna";
+    official: "Territory of the Wallis and Futuna Islands";
+    nativeName: {
+      fra: {
+        common: "Wallis et Futuna";
+        official: "Territoire des îles Wallis et Futuna";
+      };
+    };
+  };
+  capital: ["Mata-Utu"];
+  population: 11750;
+  region: "Oceania";
+  subregion: "Polynesia";
+  tld: [".wf"];
+  currencies: {
+    XPF: {
+      name: "CFP franc";
+      symbol: "₣";
+    };
+  };
+  languages: {
+    fra: "French";
+  };
+};
